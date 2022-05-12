2270cc901b30b14934d59b00d76c0509a1056530
mescalinja <a.semyonova@icloud.com>
2022-05-13 00:01:53 +0300

    promises done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('form');
+
+const createPromise = (position, delay) => {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      // Fulfill
+      resolve(`✅ Fulfilled promise ${position} in ${delay}ms`)
+    } else {
+      // Reject
+      reject(`❌ Rejected promise ${position} in ${delay}ms`)
+    }
+  })
+};
+
+const onFormSubmit = e => {
+  e.preventDefault();
+
+  const delay = e.currentTarget.elements.delay.value;
+  const step = e.currentTarget.elements.step.value;
+  const amount = e.currentTarget.elements.amount.value;
+
+  for (let i = 0; i < amount; i += 1) {
+    setTimeout(() => {
+      createPromise(i + 1, +delay + i * +step)
+        .then(message => Notiflix.Notify.success(message))
+        .catch(message => Notiflix.Notify.failure(message));
+    }, +delay + i * +step);
+  };
+  e.currentTarget.reset();
 }
+
+form.addEventListener('submit', onFormSubmit);
